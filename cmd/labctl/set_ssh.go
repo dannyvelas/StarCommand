f6a8fd417d796e493daac82f657befaee3049e6b
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-01-08 10:37:28 -0500

    temp commit M cmd/labctl/set_ssh.go M go.mod M go.sum A internal/ssh/ssh.go

--- cmd/labctl/set_ssh.go
+++ cmd/labctl/set_ssh.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 func setSSHCmd() *cobra.Command {
 	setSSHCmd := &cobra.Command{
-		Use:   "config <host-name>",
-		Short: "Generate a JSON object of configuration values for a given host",
+		Use:   "ssh <host-name>",
+		Short: "Update the `~/.ssh/config` file to connect to a given host",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			host := args[0]
-			fmt.Printf("Generating JSON config for %s...\n", host)
 		},
 	}
 
---
+++ internal/ssh/ssh.go
@@ -0,0 +1,73 @@
+package ssh
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+
+	"github.com/dannyvelas/homelab/internal/config"
+	"github.com/kevinburke/ssh_config"
+)
+
+var ErrHostAlreadyExists = errors.New("host already exists in ssh config file")
+
+func UpdateConfig(hostName string) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("error: could not find home directory: %v")
+	}
+	path := filepath.Join(home, ".ssh", "config")
+
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0o600)
+	if err != nil {
+		return fmt.Errorf("error opening ssh config: %v")
+	}
+	defer f.Close()
+
+	cfg, err := ssh_config.Decode(f)
+	if err != nil {
+		return fmt.Errorf("error parsing ssh config: %v")
+	}
+
+	// if host already exists, return
+	for _, host := range cfg.Hosts {
+		for _, pattern := range host.Patterns {
+			if pattern.String() == hostName {
+				return ErrHostAlreadyExists
+			}
+		}
+	}
+
+	if err := config.UnmarshalInto(hostName, verbose, &sshHost); err != nil {
+	}
+
+	hostBlock := buildHostBlock(hostName)
+	if _, err := f.Seek(0, 2); err == nil {
+		f.WriteString(hostBlock)
+	}
+}
+
+func buildHostBlock(host SSHHost) string {
+	const hostTmpl = `
+Host {{ .Alias }}
+    HostName {{ .HostName }}
+    User {{ .User }}
+    IdentityFile {{ .IdentityFile }}
+		Port {{ .Port }}
+`
+
+	tmpl, err := template.New("sshConfig").Parse(hostTmpl)
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, host); err != nil {
+		panic(err)
+	}
+
+	return buf.String()
+}
