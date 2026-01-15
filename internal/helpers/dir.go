2a44421440e62cf9205e643cd464e834996417cd
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-01-15 18:13:40 -0500

    removed unnecessary fn M internal/helpers/dir.go

--- internal/helpers/dir.go
+++ internal/helpers/dir.go
@@ -3,14 +3,8 @@ package helpers
 import (
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 )
-
-func AtProjectRoot(path string) string {
-	_, file, _, _ := runtime.Caller(0)
-	return filepath.Join(filepath.Dir(file), "..", path)
-}
 
 // ExpandPath takes a path and if it has a "~/" prefix, it will expand
 // it to os.UserHomeDir()
