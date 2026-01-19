26de51a2f1640e395781727e64ea36d166dac78d
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-01-19 14:50:25 -0500

    small improvement M cmd/labctl/get_config.go M internal/app/errors.go M todo.md

--- internal/app/errors.go
+++ internal/app/errors.go
@@ -2,12 +2,9 @@ package app
 
 import (
 	"errors"
-
-	"github.com/dannyvelas/conflux"
 )
 
 var (
 	ErrInvalidArgs       = errors.New("invalid arguments")
-	ErrInvalidFields     = conflux.ErrInvalidFields
 	ErrHostAlreadyExists = errors.New("host already exists in ssh config file")
 )
