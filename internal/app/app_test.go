8cf9c368732a3b3834dbab618eb1fa079ad15218
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-01-20 19:57:47 -0500

    added check tests M internal/app/app_test.go

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -8,8 +8,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestGetConfig(t *testing.T) {
-	const sampleYAML = `admin_email: "admin@example.com"
+const testYAML = `admin_email: "admin@example.com"
 auto_update_reboot_time: "05:00"
 gateway_address: 10.0.0.1
 node_cidr_address: 10.0.0.50/24
@@ -22,12 +21,12 @@ ssh_public_key_path: "~/.ssh/id_ed25519.pub"
 ssh_user: "admin"
 `
 
+func TestGetConfig(t *testing.T) {
 	cases := []struct {
-		name                string
-		hostAlias           string
-		targets             []string
-		expectedConfig      map[string]string
-		expectedDiagnostics map[string]string
+		name           string
+		hostAlias      string
+		targets        []string
+		expectedConfig map[string]string
 	}{
 		{
 			name:      "ansible as target",
@@ -44,7 +43,6 @@ ssh_user: "admin"
 				"smtp_user":               "admin",
 				"smtp_password":           "not-a-password",
 			},
-			expectedDiagnostics: map[string]string{},
 		},
 		{
 			name:      "ssh as target",
@@ -58,7 +56,6 @@ ssh_user: "admin"
 				"ssh_port":            "17031",
 				"node_cidr_address":   "10.0.0.50/24",
 			},
-			expectedDiagnostics: map[string]string{},
 		},
 		{
 			name:      "ansible and ssh as targets",
@@ -80,13 +77,12 @@ ssh_user: "admin"
 				"ssh_public_key_path":     "~/.ssh/id_ed25519.pub",
 				"ssh_user":                "admin",
 			},
-			expectedDiagnostics: map[string]string{},
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			mockFS := fstest.MapFS{"config/all.yml": {Data: []byte(sampleYAML)}}
+			mockFS := fstest.MapFS{"config/all.yml": {Data: []byte(testYAML)}}
 
 			configMux := conflux.NewConfigMux(conflux.WithYAMLFileReader("config/all.yml", conflux.WithFileSystem(mockFS)))
 
@@ -95,7 +91,7 @@ ssh_user: "admin"
 				t.Fatalf("unexpected error initializing app: %v", err)
 			}
 
-			gotConfig, gotDiagnostics, err := a.GetConfig()
+			gotConfig, _, err := a.GetConfig()
 			if err != nil {
 				t.Fatalf("unexpected error getting config: %v", err)
 			}
@@ -110,6 +106,80 @@ ssh_user: "admin"
 				if found != value {
 					t.Errorf("error: for key %s, expected %s but found=%s.", key, value, found)
 				}
+			}
+		})
+	}
+}
+
+func TestCheckConfig(t *testing.T) {
+	cases := []struct {
+		name                string
+		hostAlias           string
+		targets             []string
+		expectedDiagnostics map[string]string
+	}{
+		{
+			name:      "ansible as target",
+			hostAlias: "proxmox",
+			targets:   []string{"ansible"},
+			expectedDiagnostics: map[string]string{
+				"admin_email":             "loaded",
+				"auto_update_reboot_time": "loaded",
+				"gateway_address":         "loaded",
+				"node_cidr_address":       "loaded",
+				"physical_nic":            "loaded",
+				"proxmox_admin_password":  "loaded",
+				"smtp_password":           "loaded",
+				"smtp_user":               "loaded",
+				"ssh_port":                "loaded",
+				"ssh_public_key_path":     "loaded",
+			},
+		},
+		{
+			name:      "ssh as target",
+			hostAlias: "proxmox",
+			targets:   []string{"ssh"},
+			expectedDiagnostics: map[string]string{
+				"node_cidr_address":   "loaded",
+				"ssh_port":            "loaded",
+				"ssh_public_key_path": "loaded",
+				"ssh_user":            "loaded",
+			},
+		},
+		{
+			name:      "ansible and ssh as targets",
+			hostAlias: "proxmox",
+			targets:   []string{"ansible", "ssh"},
+			expectedDiagnostics: map[string]string{
+				"admin_email":             "loaded",
+				"auto_update_reboot_time": "loaded",
+				"gateway_address":         "loaded",
+				"node_cidr_address":       "loaded",
+				"physical_nic":            "loaded",
+				"proxmox_admin_password":  "loaded",
+				"smtp_password":           "loaded",
+				"smtp_user":               "loaded",
+				"ssh_port":                "loaded",
+				"ssh_public_key_path":     "loaded",
+				"ssh_user":                "loaded",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockFS := fstest.MapFS{"config/all.yml": {Data: []byte(testYAML)}}
+
+			configMux := conflux.NewConfigMux(conflux.WithYAMLFileReader("config/all.yml", conflux.WithFileSystem(mockFS)))
+
+			a, err := New(configMux, tc.hostAlias, tc.targets)
+			if err != nil {
+				t.Fatalf("unexpected error initializing app: %v", err)
+			}
+
+			gotDiagnostics, err := a.CheckConfig()
+			if err != nil {
+				t.Fatalf("unexpected error getting config: %v", err)
 			}
 
 			if diff := cmp.Diff(tc.expectedDiagnostics, gotDiagnostics); diff != "" {
