fb84836106984af427191aa88f931c043b7f5fd5
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-02-03 22:19:54 -0500

    reordered M internal/app/check_test.go

--- internal/app/check_test.go
+++ internal/app/check_test.go
@@ -23,73 +23,7 @@ ssh_public_key_path: "~/.ssh/id_ed25519.pub"
 ssh_user: "admin"
 `
 
-func TestCheckError(t *testing.T) {
-	configMux := conflux.NewConfigMux()
-
-	tests := []struct {
-		name        string
-		targetArgs  []string
-		expectedErr error
-	}{
-		{
-			name:        "SSH unsupported action",
-			targetArgs:  []string{"ssh:run"},
-			expectedErr: errUnsupportedCombination,
-		},
-		{
-			name:        "SSH trailing word",
-			targetArgs:  []string{"ssh:run:here"},
-			expectedErr: errExpectedEOF,
-		},
-		{
-			name:        "SSH unrecognized action",
-			targetArgs:  []string{"ssh:ssh"},
-			expectedErr: errUnrecognizedAction,
-		},
-		{
-			name:        "Ansible unrecognized subcommand",
-			targetArgs:  []string{"ansible:hi"},
-			expectedErr: errUnrecognizedAnsibleSubCommand,
-		},
-		{
-			name:        "Multiple: SSH unsupported action and ansible unrecognized subcommand",
-			targetArgs:  []string{"ssh:run", "ansible:hi"},
-			expectedErr: errUnrecognizedAnsibleSubCommand,
-		},
-		{
-			name:        "Multiple: SSH unsupported action and ansible action instead of subcommand",
-			targetArgs:  []string{"ssh:run", "ansible:run"},
-			expectedErr: errUnrecognizedAnsibleSubCommand,
-		},
-		{
-			name:        "Multiple: SSH unsupported action and ansible missing action",
-			targetArgs:  []string{"ssh:run", "ansible:inventory"},
-			expectedErr: errExpectingAction,
-		},
-		{
-			name:        "Multiple: SSH unsupported action and ansible unsupported action",
-			targetArgs:  []string{"ssh:run", "ansible:inventory:apply"},
-			expectedErr: errUnsupportedCombination,
-		},
-		{
-			name:        "Multiple: SSH unsupported action and ansible unrecognized action",
-			targetArgs:  []string{"ssh:run", "ansible:inventory:asdf"},
-			expectedErr: errUnrecognizedAction,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, err := Check(context.Background(), configMux, "proxmox", tt.targetArgs)
-			if !errors.Is(err, tt.expectedErr) {
-				t.Fatalf("ToTargets() error was %v. wanted: %v", err, tt.expectedErr)
-				return
-			}
-		})
-	}
-}
-
-func TestCheckConfig(t *testing.T) {
+func TestCheck_Success(t *testing.T) {
 	cases := []struct {
 		name                string
 		hostAlias           string
@@ -157,6 +91,72 @@ func TestCheckConfig(t *testing.T) {
 
 			if diff := cmp.Diff(tc.expectedDiagnostics, gotDiagnostics); diff != "" {
 				t.Errorf("diagnostics mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestCheck_Error(t *testing.T) {
+	configMux := conflux.NewConfigMux()
+
+	tests := []struct {
+		name        string
+		targetArgs  []string
+		expectedErr error
+	}{
+		{
+			name:        "SSH unsupported action",
+			targetArgs:  []string{"ssh:run"},
+			expectedErr: errUnsupportedCombination,
+		},
+		{
+			name:        "SSH trailing word",
+			targetArgs:  []string{"ssh:run:here"},
+			expectedErr: errExpectedEOF,
+		},
+		{
+			name:        "SSH unrecognized action",
+			targetArgs:  []string{"ssh:ssh"},
+			expectedErr: errUnrecognizedAction,
+		},
+		{
+			name:        "Ansible unrecognized subcommand",
+			targetArgs:  []string{"ansible:hi"},
+			expectedErr: errUnrecognizedAnsibleSubCommand,
+		},
+		{
+			name:        "Multiple: SSH unsupported action and ansible unrecognized subcommand",
+			targetArgs:  []string{"ssh:run", "ansible:hi"},
+			expectedErr: errUnrecognizedAnsibleSubCommand,
+		},
+		{
+			name:        "Multiple: SSH unsupported action and ansible action instead of subcommand",
+			targetArgs:  []string{"ssh:run", "ansible:run"},
+			expectedErr: errUnrecognizedAnsibleSubCommand,
+		},
+		{
+			name:        "Multiple: SSH unsupported action and ansible missing action",
+			targetArgs:  []string{"ssh:run", "ansible:inventory"},
+			expectedErr: errExpectingAction,
+		},
+		{
+			name:        "Multiple: SSH unsupported action and ansible unsupported action",
+			targetArgs:  []string{"ssh:run", "ansible:inventory:apply"},
+			expectedErr: errUnsupportedCombination,
+		},
+		{
+			name:        "Multiple: SSH unsupported action and ansible unrecognized action",
+			targetArgs:  []string{"ssh:run", "ansible:inventory:asdf"},
+			expectedErr: errUnrecognizedAction,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Check(context.Background(), configMux, "proxmox", tt.targetArgs)
+			if !errors.Is(err, tt.expectedErr) {
+				t.Fatalf("ToTargets() error was %v. wanted: %v", err, tt.expectedErr)
+				return
 			}
 		})
 	}
