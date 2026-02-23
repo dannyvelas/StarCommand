8302e49de30f75b69008f62753ac65674ad97b4d
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-02-22 23:56:35 -0500

    added some missing fields M internal/app/ansible_setup_host_config.go

--- internal/app/ansible_setup_host_config.go
+++ internal/app/ansible_setup_host_config.go
@@ -8,6 +8,10 @@ type ansibleSetupHostConfig struct {
 	// Required
 	IncusStoragePoolName string `json:"incus_storage_pool_name" required:"true"`
 	IncusStorageDriver   string `json:"incus_storage_driver" required:"true"`
+
+	// Injected
+	SMTPUser     string `json:"smtp_user"`
+	SMTPPassword string `json:"smtp_password"`
 }
 
 func newAnsibleSetupHostConfig() *ansibleSetupHostConfig {
