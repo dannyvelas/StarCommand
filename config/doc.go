0c84c21e2d6c6a58acdfb4bbd524e109c7d0f692
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-03-03 15:43:53 -0500

    created config pkg A config/config.go A config/doc.go A config/models.go

---
+++ config/doc.go
@@ -0,0 +1,2 @@
+// Package config has the config structs and logic to unmarshal the stc.yaml config file
+package config
---
+++ config/models.go
@@ -0,0 +1,34 @@
+package config
+
+type Config struct {
+	Hosts []Host `yaml:"hosts"`
+}
+
+type Host struct {
+	Name                 string      `yaml:"name"`
+	IP                   string      `yaml:"ip"`
+	SSH                  SSHConfig   `yaml:"ssh"`
+	AutoUpdateRebootTime string      `yaml:"auto_update_reboot_time"`
+	WireGuardEndpoint    bool        `yaml:"wireguard_endpoint"`
+	Incus                IncusConfig `yaml:"incus"`
+	VMs                  []VM        `yaml:"vms"`
+}
+
+type VM struct {
+	Name                 string    `yaml:"name"`
+	IP                   string    `yaml:"ip"`
+	SSH                  SSHConfig `yaml:"ssh"`
+	AutoUpdateRebootTime string    `yaml:"auto_update_reboot_time"`
+}
+
+type SSHConfig struct {
+	User           string `yaml:"user"`
+	Port           int    `yaml:"port"`
+	PrivateKeyPath string `yaml:"private_key_path"`
+	PublicKeyPath  string `yaml:"public_key_path"`
+}
+
+type IncusConfig struct {
+	StoragePoolName   string `yaml:"storage_pool_name"`
+	StoragePoolDriver string `yaml:"storage_pool_driver"`
+}
